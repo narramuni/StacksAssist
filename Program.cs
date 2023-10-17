ea5dca02a8fab5db0b9384f9f834f3269fed9f15
narramuni <nmspavan@gmail.com>
2023-10-17 15:34:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+        public static void runApp()
+        {
+            StackPro stack = new StackPro();
+            Console.WriteLine("length of stack=" + stack.getLengthOfStack());
+            if (stack.peek() == null)
+                Console.WriteLine("Stack contains nothing");
+            for (int i = 0; i < 10; i++)
+            {
+                Node n = new Node();
+                n.Data = i.ToString();
+                stack.push(n);
+                Console.WriteLine("stack Length=" + stack.getLengthOfStack());
+            }
+
+            Node node = stack.pop();
+            if (node != null)
+
+                Console.WriteLine("Popped " + node.Data);
+
+            node = stack.pop();
+            if (node != null)
+                Console.WriteLine("Popped " + node.Data);
+
+            Console.WriteLine("Stack Contents: " + stack.listContents());
+            Console.ReadLine(); 
+
+        }
+    }
+}
+
+
+
+
